e02f9c0699e11ab686747a693898c60fcd58fc84
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-04 12:46:11 +0800

    Java Exam: modified add of StringCalculator

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -2,6 +2,19 @@ package com.tw;
 
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        int sum = 0;
+        String cur_num = "";
+        for(int i = 0; i < string.length(); i++){
+            if(Character.isDigit(string.charAt(i))){
+                cur_num += String.valueOf(string.charAt(i));
+            }else if(cur_num != ""){
+                sum += Integer.parseInt(cur_num);
+                cur_num = "";
+            }
+        }
+        if(cur_num != "")
+            sum += Integer.parseInt(cur_num);
+
+        return sum;
     }
 }
